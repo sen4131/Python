63346adc1a649bcedb5b62d6702f578471b69f9b
Sen <zenzosen@gmail.com>
2017-10-09 16:03:11 -0400

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct  7 15:48:10 2017
+@author: Sen Varghese
+@subject: CUS 620
+@title : Evens & odds
 
-@author: Sen
 """
 def main():
     ''' 
@@ -24,18 +26,21 @@ def main():
             x = int(input('\nEnter a number (0 terminates): '))
         except:
             print('\nplease enter an integer')
-        if x > 0:
+            x = ''
+        if x > 0 and type(x) == int:
             lst.append(x)
-        elif x < 0:
+        elif x < 0 and type(x) == int:
             print('\nYou\'ve entered a negative number')
-    
+
     # loop through the full user list and splits and count the numbers
     # based on whether even or odd number           
     for i in lst:
-        if i%2 == 0:
+        
+        if i % 2 == 0:
             countEven += 1
             evenList.append(i)
-        elif i%2 == 1:
+
+        elif i % 2 == 1:
             countOdd += 1
             oddList.append(i)
     
@@ -46,11 +51,12 @@ def main():
     
     
     #all the output printed for user
-    print('\nyou\'ve entered '+ str(countEven) + ' even numbers')
-    print('\nsum of even numbers: '+ str(sumEven))
-    print('\nyou\'ve entered '+ str(countOdd) + ' odd numbers') 
-    print('\nsum of odd numbers: '+ str(sumOdd))    
-    print('\nTotal of ' + str(len(lst)) + ' positive numbers')
+    print('\nAnswer:')
+    print('\n{} {} {}'.format('you\'ve entered', countEven, 'even numbers'))
+    print('\n{} {} {}'.format('you\'ve entered', countOdd, 'odd numbers'))
+    print('\n{} {}'.format('Sum of even numbers:', sumEven))
+    print('\n{} {}'.format('Sum of odd  numbers:', sumOdd))
+    print('\n{} {} {}'.format('Total of', len(lst), 'positive numbers'))
 
 #    print(lst)
 
